6434d24b3a764817f263b5cf8fbd3354f73ad489
Prodis <prodis@gmail.com>
2020-07-26 09:44:02 +0200

    Miss.Map.rename_key/3.

--- lib/miss/map.ex
+++ lib/miss/map.ex
@@ -3,20 +3,56 @@ defmodule Miss.Map do
   Functions to extend the Elixir `Map` module.
   """
 
-  @type renaming_keys :: [{actual_key :: Map.key(), new_key :: Map.key()}] | map()
+  @type keys_to_rename :: [{actual_key :: Map.key(), new_key :: Map.key()}] | map()
   @typep map_to_list :: [{Map.key(), Map.value()}]
+
+  @doc """
+  Renames a single key in the given `map`.
+
+  If `actual_key` does not exist in `map`, it is simply ignored.
+
+  If a key is renamed to an existing key, the value of the actual key remains.
+
+  ## Examples
+
+      iex> Miss.Map.rename_key(%{a: 1, b: 2, c: 3}, :b, :bbb)
+      %{a: 1, bbb: 2, c: 3}
+
+      iex> Miss.Map.rename_key(%{"a" => 1, "b" => 2, "c" => 3}, "b", "bbb")
+      %{"a" => 1, "bbb" => 2, "c" => 3}
+
+      iex> Miss.Map.rename_key(%{a: 1, b: 2, c: 3}, :z, :zzz)
+      %{a: 1, b: 2, c: 3}
+
+      iex> Miss.Map.rename_key(%{a: 1, b: 2, c: 3}, :a, :c)
+      %{b: 2, c: 1}
+
+      iex> Miss.Map.rename_key(%{a: 1, b: 2, c: 3}, :c, :a)
+      %{a: 3, b: 2}
+
+  """
+  @spec rename_key(map(), May.key(), Map.key()) :: map()
+  def rename_key(map, actual_key, new_key) when is_map(map) do
+    case :maps.take(actual_key, map) do
+      {value, new_map} -> :maps.put(new_key, value, new_map)
+      :error -> map
+    end
+  end
+
+  def rename_key(non_map, _actual_key, _new_key), do: :erlang.error({:badmap, non_map})
 
   @doc """
   Renames keys in the given `map`.
 
-  Keys to be renamed are given through `renaming_keys` that accepts either:
+  Keys to be renamed are given through `keys_to_rename` that accepts either:
   * a list of two-element tuples: `{actual_key, new_key}`; or
-  * a map where the keys are the actual keys and the values are the new keys:
-  `%{actual_key => new_key}`
+  * a map where the keys are the actual keys and the values are the new keys: `%{actual_key => new_key}`
 
-  If `renaming_keys` contains keys that are not in `map`, they are simply ignored.
+  If `keys_to_rename` contains keys that are not in `map`, they are simply ignored.
 
-  After renaming the keys, duplicated keys are removed, the preceding one prevails.
+  It is not recommended to use `#{inspect(__MODULE__)}.rename_keys/2` to rename keys to existing
+  keys. But if you do, after renaming the keys, duplicate keys are removed and the value of the
+  preceding one prevails. See the examples for more details.
 
   ## Examples
 
@@ -45,32 +81,32 @@ defmodule Miss.Map do
       %{a: 1, b: 2, c: 3}
 
   """
-  @spec rename_keys(map(), renaming_keys()) :: map()
+  @spec rename_keys(map(), keys_to_rename()) :: map()
   def rename_keys(map, []) when is_map(map), do: map
 
-  def rename_keys(map, renaming_keys) when is_map(map) and renaming_keys == %{}, do: map
+  def rename_keys(map, keys_to_rename) when is_map(map) and keys_to_rename == %{}, do: map
 
-  def rename_keys(map, renaming_keys) when is_map(map) and is_list(renaming_keys),
-    do: rename_keys(map, :maps.from_list(renaming_keys))
+  def rename_keys(map, keys_to_rename) when is_map(map) and is_list(keys_to_rename),
+    do: rename_keys(map, :maps.from_list(keys_to_rename))
 
-  def rename_keys(map, renaming_keys) when is_map(map) and is_map(renaming_keys) do
+  def rename_keys(map, keys_to_rename) when is_map(map) and is_map(keys_to_rename) do
     map
     |> :maps.to_list()
-    |> rename_keys(renaming_keys, _acc = [])
+    |> do_rename_keys(keys_to_rename, _acc = [])
   end
 
-  def rename_keys(non_map, _renaming_keys), do: :erlang.error({:badmap, non_map})
+  def rename_keys(non_map, _keys_to_rename), do: :erlang.error({:badmap, non_map})
 
-  @spec rename_keys(map_to_list(), map(), map_to_list()) :: map()
-  defp rename_keys([], _keys_mapping, acc), do: :maps.from_list(acc)
+  @spec do_rename_keys(map_to_list(), map(), map_to_list()) :: map()
+  defp do_rename_keys([], _keys_mapping, acc), do: :maps.from_list(acc)
 
-  defp rename_keys([{key, value} | rest], keys_mapping, acc) do
+  defp do_rename_keys([{key, value} | rest], keys_mapping, acc) do
     item =
       case keys_mapping do
         %{^key => new_key} -> {new_key, value}
         %{} -> {key, value}
       end
 
-    rename_keys(rest, keys_mapping, [item | acc])
+    do_rename_keys(rest, keys_mapping, [item | acc])
   end
 end
--- test/miss/map_test.exs
+++ test/miss/map_test.exs
@@ -6,43 +6,134 @@ defmodule Miss.MapTest do
   doctest Subject
 
   setup do
-    map_keys_atom = %{
+    map_atom_keys = %{
       key1: "one",
       key2: 222,
       key3: 'tree'
     }
 
-    map_keys_string = %{
+    map_string_keys = %{
       "key1" => "one",
       "key2" => 222,
       "key3" => 'tree'
     }
 
-    {:ok, map_keys_atom: map_keys_atom, map_keys_string: map_keys_string}
+    {:ok, map_atom_keys: map_atom_keys, map_string_keys: map_string_keys}
+  end
+
+  describe "rename_key/3" do
+    test "renames atom key", %{map_atom_keys: map} do
+      assert Subject.rename_key(map, :key1, :key_one) == %{
+               key_one: "one",
+               key2: 222,
+               key3: 'tree'
+             }
+
+      assert Subject.rename_key(map, :key2, :key_two) == %{
+               key1: "one",
+               key_two: 222,
+               key3: 'tree'
+             }
+
+      assert Subject.rename_key(map, :key3, :tree) == %{
+               key1: "one",
+               key2: 222,
+               tree: 'tree'
+             }
+    end
+
+    test "renames string key", %{map_string_keys: map} do
+      assert Subject.rename_key(map, "key1", "key_one") == %{
+               "key_one" => "one",
+               "key2" => 222,
+               "key3" => 'tree'
+             }
+
+      assert Subject.rename_key(map, "key2", "key_two") == %{
+               "key1" => "one",
+               "key_two" => 222,
+               "key3" => 'tree'
+             }
+
+      assert Subject.rename_key(map, "key3", "tree") == %{
+               "key1" => "one",
+               "key2" => 222,
+               "tree" => 'tree'
+             }
+    end
+
+    test "when a given key to rename does not exist, ignores the key", %{
+      map_atom_keys: map_atom_keys,
+      map_string_keys: map_string_keys
+    } do
+      assert Subject.rename_key(map_atom_keys, :not_exist, :nothing) == map_atom_keys
+      assert Subject.rename_key(map_string_keys, "not_exist", "nothing") == map_string_keys
+    end
+
+    test "when the new key has the same name of an existing key, " <>
+           "renames the key using the value of the old key",
+         %{
+           map_atom_keys: map_atom_keys,
+           map_string_keys: map_string_keys
+         } do
+      assert Subject.rename_key(map_atom_keys, :key1, :key2) == %{
+               key2: "one",
+               key3: 'tree'
+             }
+
+      assert Subject.rename_key(map_string_keys, "key3", "key1") == %{
+               "key1" => 'tree',
+               "key2" => 222
+             }
+    end
+
+    test "when a map is not given, raises a BadMapError" do
+      assert_raise BadMapError, fn ->
+        Subject.rename_key("not a map", :key1, :one)
+      end
+    end
   end
 
   describe "rename_keys/2" do
-    test "renames atom keys", %{map_keys_atom: map} do
-      assert Subject.rename_keys(map, key1: :key_one) ==
-               %{key_one: "one", key2: 222, key3: 'tree'}
+    test "renames atom keys", %{map_atom_keys: map} do
+      assert Subject.rename_keys(map, key1: :key_one) == %{
+               key_one: "one",
+               key2: 222,
+               key3: 'tree'
+             }
 
-      assert Subject.rename_keys(map, %{key1: :key_one}) ==
-               %{key_one: "one", key2: 222, key3: 'tree'}
+      assert Subject.rename_keys(map, %{key1: :key_one}) == %{
+               key_one: "one",
+               key2: 222,
+               key3: 'tree'
+             }
 
-      assert Subject.rename_keys(map, key1: :key_one, key2: :key_two) ==
-               %{key_one: "one", key_two: 222, key3: 'tree'}
+      assert Subject.rename_keys(map, key1: :key_one, key2: :key_two) == %{
+               key_one: "one",
+               key_two: 222,
+               key3: 'tree'
+             }
 
-      assert Subject.rename_keys(map, %{key1: :key_one, key2: :key_two}) ==
-               %{key_one: "one", key_two: 222, key3: 'tree'}
+      assert Subject.rename_keys(map, %{key1: :key_one, key2: :key_two}) == %{
+               key_one: "one",
+               key_two: 222,
+               key3: 'tree'
+             }
 
-      assert Subject.rename_keys(map, key1: :key_one, key2: :key_two, key3: :tree) ==
-               %{key_one: "one", key_two: 222, tree: 'tree'}
+      assert Subject.rename_keys(map, key1: :key_one, key2: :key_two, key3: :tree) == %{
+               key_one: "one",
+               key_two: 222,
+               tree: 'tree'
+             }
 
-      assert Subject.rename_keys(map, %{key1: :key_one, key2: :key_two, key3: :tree}) ==
-               %{key_one: "one", key_two: 222, tree: 'tree'}
+      assert Subject.rename_keys(map, %{key1: :key_one, key2: :key_two, key3: :tree}) == %{
+               key_one: "one",
+               key_two: 222,
+               tree: 'tree'
+             }
     end
 
-    test "renames string keys", %{map_keys_string: map} do
+    test "renames string keys", %{map_string_keys: map} do
       assert Subject.rename_keys(map, [
                {"key1", "key_one"}
              ]) == %{
@@ -99,30 +190,28 @@ defmodule Miss.MapTest do
     end
 
     test "when a given key to rename does not exist, ignores the key", %{
-      map_keys_atom: map_keys_atom,
-      map_keys_string: map_keys_string
+      map_atom_keys: map_atom_keys,
+      map_string_keys: map_string_keys
     } do
-      assert Subject.rename_keys(map_keys_atom, not_exist: :nothing) == map_keys_atom
-      assert Subject.rename_keys(map_keys_atom, %{not_exist: :nothing}) == map_keys_atom
+      assert Subject.rename_keys(map_atom_keys, not_exist: :nothing) == map_atom_keys
+      assert Subject.rename_keys(map_atom_keys, %{not_exist: :nothing}) == map_atom_keys
 
-      assert Subject.rename_keys(map_keys_atom, not_exist: :nothing, key3: :something) ==
-               %{
-                 key1: "one",
-                 key2: 222,
-                 something: 'tree'
-               }
+      assert Subject.rename_keys(map_atom_keys, not_exist: :nothing, key3: :something) == %{
+               key1: "one",
+               key2: 222,
+               something: 'tree'
+             }
 
-      assert Subject.rename_keys(map_keys_atom, %{not_exist: :nothing, key3: :something}) ==
-               %{
-                 key1: "one",
-                 key2: 222,
-                 something: 'tree'
-               }
+      assert Subject.rename_keys(map_atom_keys, %{not_exist: :nothing, key3: :something}) == %{
+               key1: "one",
+               key2: 222,
+               something: 'tree'
+             }
 
-      assert Subject.rename_keys(map_keys_string, [{"not_exist", "nothing"}]) == map_keys_string
-      assert Subject.rename_keys(map_keys_string, %{"not_exist" => "nothing"}) == map_keys_string
+      assert Subject.rename_keys(map_string_keys, [{"not_exist", "nothing"}]) == map_string_keys
+      assert Subject.rename_keys(map_string_keys, %{"not_exist" => "nothing"}) == map_string_keys
 
-      assert Subject.rename_keys(map_keys_string, [
+      assert Subject.rename_keys(map_string_keys, [
                {"not_exist", "nothing"},
                {"key1", "something"}
              ]) == %{
@@ -131,7 +220,7 @@ defmodule Miss.MapTest do
                "key3" => 'tree'
              }
 
-      assert Subject.rename_keys(map_keys_string, %{
+      assert Subject.rename_keys(map_string_keys, %{
                "not_exist" => "nothing",
                "key1" => "something"
              }) == %{
@@ -141,45 +230,41 @@ defmodule Miss.MapTest do
              }
     end
 
-    test "when a new key has the same of an existing key, " <>
-           "renames the key and removes the duplicated keys using the value of the preceding one",
+    test "when a new key has the same name of an existing key, " <>
+           "renames the keys and removes the duplicated keys using the value of the preceding one",
          %{
-           map_keys_atom: map_keys_atom,
-           map_keys_string: map_keys_string
+           map_atom_keys: map_atom_keys,
+           map_string_keys: map_string_keys
          } do
-      assert Subject.rename_keys(map_keys_atom, key1: :key2) ==
-               %{
-                 key2: "one",
-                 key3: 'tree'
-               }
+      assert Subject.rename_keys(map_atom_keys, key1: :key2) == %{
+               key2: "one",
+               key3: 'tree'
+             }
 
-      assert Subject.rename_keys(map_keys_atom, %{key1: :key2}) ==
-               %{
-                 key2: "one",
-                 key3: 'tree'
-               }
+      assert Subject.rename_keys(map_atom_keys, %{key1: :key2}) == %{
+               key2: "one",
+               key3: 'tree'
+             }
 
-      assert Subject.rename_keys(map_keys_string, [{"key3", "key1"}]) ==
-               %{
-                 "key1" => "one",
-                 "key2" => 222
-               }
+      assert Subject.rename_keys(map_string_keys, [{"key3", "key1"}]) == %{
+               "key1" => "one",
+               "key2" => 222
+             }
 
-      assert Subject.rename_keys(map_keys_string, %{"key3" => "key1"}) ==
-               %{
-                 "key1" => "one",
-                 "key2" => 222
-               }
+      assert Subject.rename_keys(map_string_keys, %{"key3" => "key1"}) == %{
+               "key1" => "one",
+               "key2" => 222
+             }
     end
 
-    test "when an empty keys are given, returns the same map", %{
-      map_keys_atom: map_keys_atom,
-      map_keys_string: map_keys_string
+    test "when empty keys are given, returns the same map", %{
+      map_atom_keys: map_atom_keys,
+      map_string_keys: map_string_keys
     } do
-      assert Subject.rename_keys(map_keys_atom, []) == map_keys_atom
-      assert Subject.rename_keys(map_keys_atom, %{}) == map_keys_atom
-      assert Subject.rename_keys(map_keys_string, []) == map_keys_string
-      assert Subject.rename_keys(map_keys_string, %{}) == map_keys_string
+      assert Subject.rename_keys(map_atom_keys, []) == map_atom_keys
+      assert Subject.rename_keys(map_atom_keys, %{}) == map_atom_keys
+      assert Subject.rename_keys(map_string_keys, []) == map_string_keys
+      assert Subject.rename_keys(map_string_keys, %{}) == map_string_keys
     end
 
     test "when a map is not given, raises a BadMapError" do
